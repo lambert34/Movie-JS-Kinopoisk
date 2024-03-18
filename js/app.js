c7e6c74e14254ec402f32925aa8081bc586c936a
alexL <aleksandrlambert@gmail.com>
2024-03-18 19:27:51 +0700

    добавляет функцию для вставки фильма в верстку
    добавляет объект массива со значением фильма

--- js/app.js
+++ js/app.js
@@ -5,7 +5,7 @@ const API_URL_SEARCH =
     "https://kinopoiskapiunofficial.tech/api/v2.1/films/search-by-keyword?keyword=";
 
 getMovies(API_URL_POPULAR);
-    async function getMovies(url) {
+async function getMovies(url) {
     const resp = await fetch(url, {
         headers: {
             "Content-Type": "application/json",
@@ -13,4 +13,29 @@ getMovies(API_URL_POPULAR);
         },
     });
     const respData = await resp.json();
+    showMovies(respData)
+}
+
+// функция для отрисовки всех карточек 
+
+function showMovies(data) {
+    const moviesEl = document.querySelector(".movies");
+
+    data.films.forEach((movie) => {
+        const movieEl = documenr.createElement("div");
+        movieEl.classList.add("movie");
+        movieEl.innerHTML = `
+        <div class="movie__cover-inner">
+        <img class="movie__cover"
+            src="https://upload.wikimedia.org/wikipedia/ru/2/2e/%D0%9C%D0%BE%D1%80%D1%82%D0%B0%D0%BB_%D0%9A%D0%BE%D0%BC%D0%B1%D0%B0%D1%82.png"
+            alt="">
+        <div class="movie__cover--darkened"></div>
+    </div>
+    <div class="movie__info">
+        <div class="movie__title">Mortal Kombat</div>
+        <div class="movie__category">Фантастика</div>
+        <div class="movie__average movie__average--green">9</div>
+    </div>
+        `;
+    });
 }
