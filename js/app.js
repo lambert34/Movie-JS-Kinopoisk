5d232db5acc7de8b65d1665e0cc973a03738c86b
alexL <aleksandrlambert@gmail.com>
2024-03-18 22:14:43 +0700

    добавляет модальное окно

--- js/app.js
+++ js/app.js
@@ -3,6 +3,7 @@ const API_URL_POPULAR =
     "https://kinopoiskapiunofficial.tech/api/v2.2/films/top?type=TOP_100_POPULAR_FILMS&page=1";
 const API_URL_SEARCH =
     "https://kinopoiskapiunofficial.tech/api/v2.1/films/search-by-keyword?keyword=";
+const API_URL_MOVIE_DETAILS = "https://kinopoiskapiunofficial.tech/api/v2.2/films/"
 
 getMovies(API_URL_POPULAR);
 
@@ -17,26 +18,20 @@ async function getMovies(url) {
     showMovies(respData);
 }
 
-
-// условие для цвета рейтинга
-
 function getClassByRate(vote) {
     if (vote >= 7) {
         return "green";
-    } else if (vote >= 5) {
+    } else if (vote > 5) {
         return "orange";
     } else {
         return "red";
     }
 }
 
-// функция для отрисовки всех карточек 
-
 function showMovies(data) {
     const moviesEl = document.querySelector(".movies");
 
-
-    // очистка предыдущей информации
+    // Очищаем предыдущие фильмы
     document.querySelector(".movies").innerHTML = "";
 
     data.films.forEach((movie) => {
@@ -44,21 +39,30 @@ function showMovies(data) {
         movieEl.classList.add("movie");
         movieEl.innerHTML = `
         <div class="movie__cover-inner">
-        <img 
-        class="movie__cover"
+        <img
         src="${movie.posterUrlPreview}"
-        alt="${movie.nameRu}">
+        class="movie__cover"
+        alt="${movie.nameRu}"
+        />
         <div class="movie__cover--darkened"></div>
     </div>
     <div class="movie__info">
         <div class="movie__title">${movie.nameRu}</div>
-        <div class="movie__category">${movie.genres.map((genre) => ` ${genre.genre}`
+        <div class="movie__category">${movie.genres.map(
+            (genre) => ` ${genre.genre}`
         )}</div>
-        <div class="movie__average movie__average--${getClassByRate(movie.rating)}">${movie.rating}</div>
+        ${movie.rating &&
+            `
+        <div class="movie__average movie__average--${getClassByRate(
+                movie.rating
+            )}">${movie.rating}</div>
+        `
+            }
     </div>
         `;
+        movieEl.addEventListener("click", () => openModal(movie.filmId))
         moviesEl.appendChild(movieEl);
-    }); 
+    });
 }
 
 const form = document.querySelector("form");
@@ -72,6 +76,58 @@ form.addEventListener("submit", (e) => {
         getMovies(apiSearchUrl);
 
         search.value = "";
-        // делает пустой форму поиска 
     }
 });
+
+// Modal
+const modalEl = document.querySelector(".modal");
+
+async function openModal(id) {
+    const resp = await fetch(API_URL_MOVIE_DETAILS + id, {
+        headers: {
+            "Content-Type": "application/json",
+            "X-API-KEY": API_KEY,
+        },
+    });
+    const respData = await resp.json();
+
+    modalEl.classList.add("modal--show");
+    document.body.classList.add("stop-scrolling");
+
+    modalEl.innerHTML = `
+    <div class="modal__card">
+    <img class="modal__movie-backdrop" src="${respData.posterUrl}" alt="">
+    <h2>
+        <span class="modal__movie-title">${respData.nameRu}</span>
+        <span class="modal__movie-release-year"> - ${respData.year}</span>
+    </h2>
+    <ul class="modal__movie-info">
+        <div class="loader"></div>
+        <li class="modal__movie-genre">Жанр - ${respData.genres.map((el) => `<span>${el.genre}</span>`)}</li>
+        ${respData.filmLength ? `<li class="modal__movie-runtime">Время - ${respData.filmLength} минут</li>` : ''}
+        <li >Сайт: <a class="modal__movie-site" href="${respData.webUrl}">${respData.webUrl}</a></li>
+        <li class="modal__movie-overview">Описание - ${respData.description}</li>
+    </ul>
+    <button type="button" class="modal__button-close">Закрыть</button>
+    </div>
+`
+    const btnClose = document.querySelector(".modal__button-close");
+    btnClose.addEventListener("click", () => closeModal());
+}
+
+function closeModal() {
+    modalEl.classList.remove("modal--show");
+    document.body.classList.remove("stop-scrolling");
+}
+
+window.addEventListener("click", (e) => {
+    if (e.target === modalEl) {
+        closeModal();
+    }
+})
+
+window.addEventListener("keydown", (e) => {
+    if (e.keyCode === 27) {
+        closeModal();
+    }
+})
